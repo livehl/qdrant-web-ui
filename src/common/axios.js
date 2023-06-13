d58a8ce6b4b77372136daea61e0ae7b5e4ffa556
huang.lin <hh@chaintool.ai>
2023-06-13 16:52:39 +0800

    add docker file

--- src/common/axios.js
+++ src/common/axios.js
@@ -3,6 +3,8 @@ import axios from "axios";
 function setupAxios({apiKey}) {
   if (process.env.NODE_ENV === "development") {
     axios.defaults.baseURL = "http://localhost:6333";
+  }else{
+    axios.defaults.baseURL = window.location.href;
   }
   if (apiKey) {
     axios.defaults.headers.common["api-key"] = apiKey;
